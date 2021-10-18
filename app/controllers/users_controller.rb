9ae52daa91fdc219fef1baf9083061dd15d2e93a
willy-wonka-git <lavaka999@gmail.com>
2021-10-18 18:21:36 +0300

    Sort users by nickname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page params[:page]
+    @users = User.order(:nickname).page params[:page]
   end
 
   def show
