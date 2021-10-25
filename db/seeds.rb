33254f3c541fdb2fcf69177f40123c8c79092721
willy-wonka-git <lavaka999@gmail.com>
2021-10-25 19:15:31 +0300

    Seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,36 @@
-# # Create user admin
-# User.create(
-#   nickname: "admin",
-#   role: "admin",
-#   email: "admin@gmail.com",
-#   password: "111111"
-# )
+# Create user admin
+User.create(
+  nickname: "admin",
+  role: "admin",
+  email: "admin@gmail.com",
+  password: "111111"
+)
 
-# # Create categories
-# 12.times do
-#   PostCategory.create(
-#     title: Faker::Hobby.activity,
-#     description: Faker::Lorem.paragraph
-#   )
-# end
+# Create categories
+12.times do
+  PostCategory.create(
+    title: Faker::Hobby.activity,
+    description: Faker::Lorem.paragraph
+  )
+end
 
-# # Create tags
-# 20.times do
-#   Tag.create(name: Faker::Superhero.power)
-#   Tag.create(name: Faker::Food.fruits)
-# end
+# Create tags
+20.times do
+  Tag.create(name: Faker::Superhero.power)
+  Tag.create(name: Faker::Food.fruits)
+end
 
-# # Create users
-# 80.times do
-#   User.create(
-#     nickname: Faker::Internet.user_name,
-#     fullname: Faker::Name.name,
-#     role: "user",
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password,
-#     description: Faker::Lorem.paragraph
-#   )
-# end
+# Create users
+80.times do
+  User.create(
+    nickname: Faker::Internet.user_name,
+    fullname: Faker::Name.name,
+    role: "user",
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    description: Faker::Lorem.paragraph
+  )
+end
 
 # Create posts
 users = User.order('RANDOM()').take(10)
