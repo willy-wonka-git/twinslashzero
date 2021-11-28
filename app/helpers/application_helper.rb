48d93e311128f4fb42a67d4039a8599a2f4b902a
willy-wonka-git <lavaka999@gmail.com>
2021-11-28 18:38:13 +0100

    Fix travis tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,4 @@ module ApplicationHelper
     base_title = 'Ads'
     title.empty? ? base_title : [title, base_title].join(' | ')
   end
-
-  def cloudinary_asset_url(image)
-    Cloudinary::Utils.cloudinary_url(image, type: :asset)
-  end
 end
